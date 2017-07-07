28fa9941e7492478a86a00d721e1cc33076c2c3e
Paulo Chaves <opaulochaves@gmail.com>
2017-07-07 17:07:19 -0300

    add Material-UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './index.css'
 
 import React from 'react'
 import {render} from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 import App from './App'
 
